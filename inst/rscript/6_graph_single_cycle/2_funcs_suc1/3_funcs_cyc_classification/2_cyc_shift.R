c85e7ab51edce0e37b211926b59ff32e3f96db8e
zxzhang <3257649830@qq.com>
2022-05-14 10:27:28 +0800

    May 14

--- inst/rscript/6_graph_single_cycle/2_funcs_suc1/3_funcs_cyc_classification/2_cyc_shift.R
+++ inst/rscript/6_graph_single_cycle/2_funcs_suc1/3_funcs_cyc_classification/2_cyc_shift.R
@@ -182,8 +182,8 @@ cyc_shift_main <- function(res_path, input_tumor_name) {
   dir.create(file.path(res_path, res_sub_path), recursive = TRUE, showWarnings = FALSE)
   res_file_path = file.path(res_path, res_sub_path, "cycle_shift_path_df_list.RData")
 
-  save(cycle_shift_path_df_list, file="E:/scFEA_universal/my_R/aimA/rdata_cycle_detect/6_graph_single_cycle/2_funcs_suc1/3_funcs_cyc_classification/cycle_shift_path_df_list.RData")
-
+  #save(cycle_shift_path_df_list, file="E:/scFEA_universal/my_R/aimA/rdata_cycle_detect/6_graph_single_cycle/2_funcs_suc1/3_funcs_cyc_classification/cycle_shift_path_df_list.RData")
+  save(cycle_shift_path_df_list, file=res_file_path)
 }
 
 
